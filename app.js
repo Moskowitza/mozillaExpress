21c7f60cf01e81ee0908e9eef9c83c6712127504
moskowitza <moskowitzaaron@mac.com>
2018-10-08 10:45:13 -0400

    local stash

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,12 +14,15 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+//error logger and whatever we need cors for
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Router
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/catalog',catalogRouter);
